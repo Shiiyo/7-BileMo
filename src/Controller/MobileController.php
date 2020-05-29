6d8fd77ecde8e3932f1f0f848c8c9c0ed30a66ac
Shiiyo <camaille13@hotmail.fr>
2020-05-29 17:11:32 +0200

    Show the mobile list

--- src/Controller/MobileController.php
+++ src/Controller/MobileController.php
@@ -12,14 +12,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MobileController extends AbstractController
 {
     /**
-     * @Route("/mobiles/{id}", name="mobile_show", methods={"GET"})
+     * @Route("/mobiles/{id}", name="mobile_show", methods={"GET"}, requirements={"id"="\d+"})
      */
     public function showAction(SerializerInterface $serializer, MobileRepository $repo, $id)
     {
         $mobile = $repo->findOneById($id);
 
         $data = $serializer->serialize($mobile, 'json');
-        
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+ 
+        return $response;
+    }
+
+    /**
+     * @Route("/mobiles", name="mobile_list", methods={"GET"})
+     */
+    public function listAction(SerializerInterface $serializer, MobileRepository $repo)
+    {
+        $mobiles = $repo->findAll();
+
+        $data = $serializer->serialize($mobiles, 'json');
+
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
 
