36f6c8c2ddbe77a03328a1bd01e5c8c037758a97
Shiiyo <camaille13@hotmail.fr>
2020-07-03 10:20:01 +0200

    Install the lexik jwt authentication and parameter the bundle in way to generate a token

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="user_create", methods={"POST"})
+     */
+    public function createAction(SerializerInterface $serializer, Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $newUser = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        //Hash password
+        $plainPassword = $newUser->getPassword();
+        $hashPassword = $encoder->encodePassword($newUser, $plainPassword);
+        $newUser->setPassword($hashPassword);
+
+        $manager->persist($newUser);
+        $manager->flush();
+
+        return new Response("User created !", 201);
+    }
+}
+
