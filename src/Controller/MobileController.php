d5f51fe55873fed0c76133566245f84a039e1c54
Shiiyo <camaille13@hotmail.fr>
2020-07-03 10:44:40 +0200

    Protect with token all the API

--- src/Controller/MobileController.php
+++ src/Controller/MobileController.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use App\Repository\MobileRepository;
 use App\Normalizer\Normalizer as Normalizer;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class MobileController extends AbstractController
 {
